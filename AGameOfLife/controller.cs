7f394a62f34b0088e00352e30d427a3bc1963163
Peer202 <Peer200@gmx.de>
2017-06-18 12:29:23 +0200

    Add files via upload
    Basic player controller added
    Basic Helath system added

---
+++ AGameOfLife/controller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controller : MonoBehaviour {
+	public Vector2 movef;
+	public Vector2 moveb;
+	public Vector2 movel;
+	public Vector2 mover;
+	public Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+		
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ( Input.GetAxis("Vertical") > 0){rb.position = rb.position + movef * Time.deltaTime;}
+		if ( Input.GetAxis("Vertical") < 0){rb.position = rb.position + moveb * Time.deltaTime;}
+		if ( Input.GetAxis("Horizontal") < 0){rb.position = rb.position + movel * Time.deltaTime;}
+		if ( Input.GetAxis("Horizontal") > 0){rb.position = rb.position + mover * Time.deltaTime;}
+
+	}
+}
---
+++ AGameOfLife/pHealth.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pHealth : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		long phealth = 1000;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (pHealth = 0) {
+			ptod ();
+		}
+	}
+
+	void ptod(){
+		Debug.Log ("Du bist Tod!");
+
+	
+	}
+
+}
